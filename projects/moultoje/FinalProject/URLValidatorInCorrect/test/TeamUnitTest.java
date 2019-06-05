7d186f1b4552d77a14457366f9a76ae46a8bcdd6
CroomOSU <37274399+CroomOSU@users.noreply.github.com>
2019-06-04 18:35:38 -0700

    updated Validator added url files and Updated template

--- projects/moultoje/FinalProject/URLValidatorInCorrect/test/TeamUnitTest.java
+++ projects/moultoje/FinalProject/URLValidatorInCorrect/test/TeamUnitTest.java
@@ -25,25 +25,62 @@ public class TeamUnitTest extends TestCase {
 
 	//test good urls
 	public void testGoodUrls()  throws IOException {
-		UrlValidator urlVal = new UrlValidator(null, null, 1);
-		List<String> goodUrls = Files.readAllLines(new File("goodUrls.txt").toPath(), Charset.defaultCharset());
-		for (int i = 0; i < goodUrls.size(); i++) {
-			System.out.println(goodUrls.get(i));
-			boolean result = urlVal.isValid(goodUrls.get(i));
-			assertEquals(goodUrls.get(i), true, result);
+		try {
+			UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+			List<String> goodUrls = Files.readAllLines(new File("goodUrls.txt").toPath(), Charset.defaultCharset());
+			System.out.println("Testing: " + goodUrls.size() + " good URLS");
+			int count = 0;
+			for (int i = 0; i < goodUrls.size(); i++) {
+				//System.out.println(i+1);
+				//System.out.println((i + 1) + ":  " + goodUrls.get(i));
+				
+				boolean result = urlVal.isValid(goodUrls.get(i));
+				try {
+					assertEquals(goodUrls.get(i), true, result);	
+					count++;
+				}
+				catch (Exception e)
+				{
+					System.out.println(e.toString());
+				}
+			}
+			System.out.println("Completed: " + count + " good URLS");
 		}
+		catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		
 
 	}
 
 	//test bad urls
 	public void testBadUrls() throws IOException {
 		//set up the validator
-		UrlValidator urlVal = new UrlValidator(null, null, 1);
-		List<String> badUrls = Files.readAllLines(new File("badUrls.txt").toPath(), Charset.defaultCharset());
-		for (int i = 0; i < badUrls.size(); i++) {
-			System.out.println(badUrls.get(i));
-			boolean result = urlVal.isValid(badUrls.get(i));
-			assertEquals(badUrls.get(i), false, result);
+	
+		try {
+			UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+			List<String> badUrls = Files.readAllLines(new File("badUrls.txt").toPath(), Charset.defaultCharset());
+			System.out.println("Testing: " + badUrls.size() + " bad URLS");
+			int count = 0;
+			for (int i = 0; i < badUrls.size(); i++) {
+				//System.out.println(i+1);
+				//System.out.println((i + 1) + ":  " + badUrls.get(i));
+				
+				boolean result = urlVal.isValid(badUrls.get(i));
+				try {
+					assertEquals(badUrls.get(i), false, result);	
+					count++;
+				}
+				catch (Exception e)
+				{
+					System.out.println(e.toString());
+				}
+			}
+			System.out.println("Completed: " + count + " bad URLS");
+		}
+		catch (Exception e) {
+			System.out.println(e.toString());
 		}
 
 	}
